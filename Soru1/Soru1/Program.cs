9cd6fda4d6e0a6a22b4ec2d304d861e77ad658eb
Yiğit Bekiroğlu <124576682+Yigitbekiroglu07@users.noreply.github.com>
2024-01-26 19:44:56 +0300

    Algoritma Soruları

---
+++ Soru1/Soru1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace Soru1;
+// İki sayının toplamını veren uygulamayı yazınız.
+class Program
+{
+    static void Main(String[] args)
+    {
+        try // Sayı yerine harf veya sözcük girilirse kullanıcıya doğru değer girilmesi yönünde uyarı vermesi için kullanıldı
+        {
+           Console.WriteLine("Lütfen sayi 1 değerini giriniz.");
+           int sayi1 = Convert.ToInt32(Console.ReadLine()); //String'i integer'a çevirir.
+           Console.WriteLine("Lütfen sayi 2 değerini giriniz.");
+           int sayi2 = int.Parse(Console.ReadLine());  // İlgili değeri integer'a çevirir.
+           Console.WriteLine("Toplam: " + (sayi1 + sayi2));
+           // Console.ReadLine komutu, kullanıcığının yazmış olduğu değeri string olarak 
+           // ekrana getirir. Programı bekleten bir komuttur.
+        }
+        catch 
+        {
+            Console.WriteLine("Lütfen doğru bir değer giriniz.");
+        }   
+        
+    }
+}
---
+++ Soru2/Soru2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace Soru2
+{
+// Kullanıcının girdiği iki sayının karelerinin toplamını veren uygulamayı yazınız. 
+    class Program
+    {
+        static void Main(String[] args)
+        {
+            // Math.Pow(2,3) >>> 2'nin 3. kuvveti
+            Console.WriteLine("Lütfen 1. ve 2. sayilarini giriniz");
+            double sonuc = Math.Pow(int.Parse(Console.ReadLine()), 2) +
+                           Math.Pow(int.Parse(Console.ReadLine()), 2);
+            Console.WriteLine(sonuc);
+            // Pow double olduuğu için sonuc double dönecektir.
+        }
+    }
+}
