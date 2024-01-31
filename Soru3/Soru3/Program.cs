bcb8a1147f2cd32742b58a157a553076d443ea4c
Yiğit Bekiroğlu <124576682+Yigitbekiroglu07@users.noreply.github.com>
2024-01-31 12:33:11 +0300

    Algoritma Soruları

---
+++ Soru3/Soru3/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace Soru3
+{  
+    // 1'den 10'a kadar olan sayıların küplerin toplamını bulan uygulamayı yazınız.
+    class Program
+    {
+        
+        static void Main(String[] args)
+        {
+            double toplamSonucu = 0;
+            // Süslü parantez içinde yazılmama sebebi, yazılırsa
+            // her seferinde toplamSonucu 0 olarak kabul edecek ve toplamayacak.
+            for (int i = 1; i < 10; i++)
+            {
+                
+                toplamSonucu += Math.Pow(i, 3);
+                    
+            }
+            Console.WriteLine(toplamSonucu);
+        }
+        
+    }
+}
---
+++ Soru4/Soru4/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace Soru4
+{   // Doğum tarihi girilen kişinin yaşını hesaplayan uygulamayı yazınız.
+    class Program
+    {
+        static void Main(String[] args)
+        {
+            Console.WriteLine("Lütfen doğum tarihinizi girin.");
+            DateTime dogumtarihi  = DateTime.Parse(Console.ReadLine()); // String'i DateTime veri tipine dönüştür
+
+            int gün = (DateTime.Now - dogumtarihi).Days; // Bugünün tarihinden doğum tarihini çıkar.  TimeSpan'in Days properties'i bu işlemi gerçekleştirir.
+
+            int yas = gün / 365; // Kullanıcının yaşını bulma
+
+            int kalan = gün % 365; // Kullanıcının bir sonraki yaşı için kalan gününü hesaplama
+
+            // String Interpolation
+
+            Console.WriteLine($"Yaş : {yas} | {yas + 1} yaşınıza kalan gün sayısı: {365 +  (yas * 1/4)  - kalan}"); // yas * 1/4 Şubat'ın 4 yılda bir 29 çekmesinden kaynaklı.
+        }
+    }
+}
