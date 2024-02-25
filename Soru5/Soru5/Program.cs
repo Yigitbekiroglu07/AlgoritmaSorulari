020578cac41d8aab6d10e1731e2c46e9e0e26675
Yiğit Bekiroğlu <124576682+Yigitbekiroglu07@users.noreply.github.com>
2024-02-25 22:35:39 +0300

    Algoritma Soruları

---
+++ Soru5/Soru5/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace Soru5;
+
+class Program
+{
+    static void Main(String[] args)
+    {
+        // Girilen sayının faktöriyelini hesaplayan uygulamayı yazınız.
+        Console.WriteLine("Lütfen bir sayi giriniz");
+        int sayi = int.Parse(Console.ReadLine());
+        int sonuc = 1;
+        for (int i = sayi; i > 0; i--)
+        {
+            sonuc *= i;
+        }
+
+        Console.WriteLine(sonuc);
+    }
+}
