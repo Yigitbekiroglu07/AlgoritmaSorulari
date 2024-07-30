f386a1c7d12a9397ead869f3b6c39f82da02446f
Yigit Alperen Bekiroglu <yigit_alperen_1907@hotmail.com>
2024-07-30 16:17:45 +0300

    AlgoritmaSorulari

---
+++ soru8/soru8/Program.cs
@@ -0,0 +1,113 @@
+﻿using System.Diagnostics;
+using System.Reflection.Metadata.Ecma335;
+
+namespace soru8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Girilen pozitif sayının kaç basamaklı oldığunu söyleyen uygulamayı yazınız
+            #region Çözüm1
+            //Console.WriteLine("lütfen sayiyi giriniz");
+            //float sayi = float.Parse(Console.ReadLine());
+            //int sayac = 0;
+            //for(; sayi>=10;) // For döngüsü böyle kullanılabilir.
+            //{
+            //    sayi/=10;
+            //    sayac++;
+            //}
+            //if (sayi < 10)
+            //{
+            //    sayac = 1;
+
+            //}
+
+            //Console.WriteLine(sayac);
+
+            #endregion
+            #region Çözüm2
+            //Console.WriteLine("lütfen sayiyi giriniz");
+            //float sayi = float.Parse(Console.ReadLine());
+            //int sayac = 0;
+            //float _sayi = sayi;
+            //for(; ; ) // Sonsuz döngü
+            //{
+            //    sayi /= 10;
+            //    sayac++;
+            //    if (sayi < 10)
+            //        break;
+            //}
+            //sayac++;
+            //Console.WriteLine($"{_sayi} {sayac} basamaklıdır.");
+            #endregion
+            #region Çözüm3
+            //Console.WriteLine("lütfen sayiyi giriniz");
+            //float sayi = float.Parse(Console.ReadLine());
+            //int sayac = 1;
+            //float _sayi = sayi;
+            //while (true) 
+            //{
+            //    sayi /= 10;
+            //    sayac++;
+            //    if (sayi < 10)
+            //        break;
+
+            //}
+            //Console.WriteLine($"{sayi} {sayac} basamaklıdır.");
+
+
+            #endregion
+            #region Çözüm4
+            //Console.WriteLine("lütfen sayiyi giriniz");
+            //float sayi = float.Parse(Console.ReadLine());
+            //int sayac = 1;
+            //float _sayi = sayi;
+            //while (sayi >= 10 ) // true yazılmayacaksa if bloğu kaldırılmalı
+            //{
+            //    sayi /= 10;
+            //    sayac++;
+            //}
+            //Console.WriteLine($"{sayi} {sayac} basamaklıdır.");
+
+            #endregion
+            #region Çözüm5
+            //Console.WriteLine("lütfen sayiyi giriniz");
+            //float sayi = float.Parse(Console.ReadLine());
+            //int sayac = 0;
+            //float _sayi = sayi;
+            //do 
+            //{
+            //    sayi /= 10;
+            //    sayac++;
+            //} 
+            //while (sayi >= 10);    
+
+
+
+            //Console.WriteLine($"{sayi} {++sayac} basamaklıdır."); // ++sayac, arttır daha sonra yazdır demek. sayac++ yazdır, daha sonra arttır demek.
+
+            #endregion
+            #region Çözüm6
+            Console.WriteLine("lütfen sayiyi giriniz");
+            int sayi = int.Parse(Console.ReadLine());
+
+            int sonuc = BasamakSayisi(sayi);    
+
+            Console.WriteLine($"{sayi} {sonuc} basamaklıdır");   
+
+            #endregion
+        }
+        
+        static int BasamakSayisi(int sayi)
+        {
+            sayi /= 10;
+            if(sayi>=10)
+            return 1 + BasamakSayisi(sayi);
+
+            return 2;
+            
+        }
+        
+    }
+}
---
+++ soru9/soru9/Program.cs
@@ -0,0 +1,70 @@
+﻿namespace soru9
+{
+     class Program
+    {
+        static void Main(string[] args)
+        {
+            // Girilen 3 basamaklı sayının basamaklarının küpleri toplamı sayının kendisine eşit olup olmadığını bulan uygulama?
+            #region Çözüm1
+            Console.WriteLine("Lütfen 3 basamaklı bir sayi giriniz");
+            int sayi = int.Parse(Console.ReadLine());
+            Console.WriteLine(Basamak(sayi));
+            if(Basamak(sayi)) 
+            {
+                int _sayi = sayi;
+                double kupToplam = 0;
+                while(true)
+                {
+                   int basamak = _sayi % 10;
+                    _sayi /= 10;
+
+                    kupToplam += Math.Pow(basamak, 3);
+                    // kuptoplam = basamak * basamak * basamak
+
+                    if (_sayi < 10)
+                    {
+                        basamak = _sayi % 10;
+                        kupToplam += Math.Pow(basamak, 3);
+                        break;
+                    }
+                        
+                }
+                if (kupToplam == sayi)
+                {
+                    Console.WriteLine("eşittir.");
+                }  
+                else
+                {
+                    Console.WriteLine("eşit değildir");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Sadece 3 basamakllı bir sayi giriniz");
+            }
+            #endregion
+        }
+        static bool Basamak(int sayi)
+        {
+            // while(sayi>=10)
+            int basamakSayisi = 1;
+            while (true)
+            {
+                basamakSayisi++;
+                sayi /= 10;
+
+                if (sayi < 10)
+                    break;
+            }
+            // if (basamakSayisi == 3)
+            // return true;
+            // return false;
+
+            return basamakSayisi == 3 ? true : false;
+        }
+
+
+    }
+
+ 
+}
